9075260c75a56e8353746c167da82db684584e2e
piotrkumala <piotrek.kumala@gmail.com>
2022-06-12 22:47:47 +0200

    move splitting data for rnn to separate function so it can be used in lstm and gru

--- models/PrepareClimateData.py
+++ models/PrepareClimateData.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def prepare_climate_data(data_path: str) -> [pd.Series, pd.DataFrame]:
@@ -11,3 +12,15 @@ def prepare_climate_data(data_path: str) -> [pd.Series, pd.DataFrame]:
     x: pd.Series = df[[col for col in df.columns if col not in ['pm10']]]
     y: pd.DataFrame = df['pm10']
     return x, y
+
+
+def prepare_climate_data_for_rnn(data_path: str) -> [pd.Series, pd.Series, pd.Series, pd.Series, pd.Series]:
+    x,y = prepare_climate_data(data_path)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    date = x_test['date']
+    x_train = x_train.loc[:, x_train.columns != 'date'].to_numpy()
+    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1).swapaxes(1, 2)
+    x_test = x_test.loc[:, x_test.columns != 'date'].to_numpy()
+    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1).swapaxes(1, 2)
+
+    return x_train, y_train, x_test, y_test, date
--- notebooks/simple_rnn/SimpleRNNRegression.py
+++ notebooks/simple_rnn/SimpleRNNRegression.py
@@ -1,25 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import Model
-from pandas import DataFrame
-from sklearn.model_selection import train_test_split
-
 from models.PlotPredictedAndRealValues import plot_predicted_and_real_values
-from models.PrepareClimateData import prepare_climate_data
+from models.PrepareClimateData import prepare_climate_data, prepare_climate_data_for_rnn
 from tensorflow import keras
 from tensorflow.keras import layers
 
 
 def simple_rnn_regression(data_path: str):
-    x, y = prepare_climate_data(data_path)
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    date = x_test['date']
-    x_train = x_train.loc[:, x_train.columns != 'date'].to_numpy()
-    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1).swapaxes(1, 2)
-    x_test = x_test.loc[:, x_test.columns != 'date'].to_numpy()
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1).swapaxes(1, 2)
+    x_train, y_train, x_test, y_test, date = prepare_climate_data_for_rnn(data_path)
 
     model: Model = keras.Sequential()
 
