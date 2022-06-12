18093ad2464b7289c86b0b32bd3478338df6a22c
piotrkumala <piotrek.kumala@gmail.com>
2022-06-12 22:37:52 +0200

    add basic version of simple rnn

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from notebooks.linear.ClimateLinearRegression import run_linear_regression_for_climate
+from notebooks.simple_rnn.SimpleRNNRegression import simple_rnn_regression
 from notebooks.tree.ClimateTreeRegression import climate_tree_regression
 
 # run_linear_regression_for_bikes('./data/Accidents_done.csv')
-run_linear_regression_for_climate('./data/climate_and_air.csv')
+# run_linear_regression_for_climate('./data/climate_and_air.csv')
 
-climate_tree_regression('data/climate_and_air.csv')
+# climate_tree_regression('data/climate_and_air.csv')
+
+simple_rnn_regression('data/climate_and_air.csv')
---
+++ models/PlotPredictedAndRealValues.py
@@ -0,0 +1,19 @@
+import matplotlib as plt
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error
+
+
+def plot_predicted_and_real_values(y_test, y_prediction, date):
+    score = r2_score(y_test, y_prediction)
+    print('r2 score is ', score)
+    print('mean_sqrt_error is ==', mean_squared_error(y_test, y_prediction))
+    print('root_mean_squared  error  of is ==', np.sqrt(mean_squared_error(y_test, y_prediction)))
+    plt.figure(figsize=(18, 8))
+    plt.scatter(date, y_test)
+    plt.scatter(date, y_prediction)
+    plt.legend(['real pm10 level', 'predicted pm10 level'])
+    plt.xlabel('Date')
+    plt.ylabel('PM10 [ug/m^3]')
+    plt.title('PM10 level (real and predicted) in Cracow from 2000 to 2021 using simple recurrent neural network '
+              'regression')
+    plt.show()
